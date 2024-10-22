7c61601bb9dae864f985022ca5da1a67a35e01d0
David Paul Solutions Architect <dpaul116@googlemail.com>
2024-10-22 20:34:57 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './HomeScreen';
-import PostScreen from './PostScreen';
-import OwlInfoScreen from './OwlInfoScreen'; 
-import ProfileScreen from './ProfileScreen';
-
-const Stack = createStackNavigator();
+import BottomTabNavigator from './BottomTabNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Post" component={PostScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="OwlInfo" component={OwlInfoScreen} />
-        <Stack.Screen name="OwlSale" component={OwlSaleScreen} />
-      </Stack.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 };
